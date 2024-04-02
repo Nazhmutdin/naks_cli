3c9260356f90e812ae3c01fbf2c9b7c706ccd9e4
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2024-04-02 17:13:39 +0300

    refactoring and removing unused code

--- src/utils/UoWs.py
+++ src/utils/UoWs.py
@@ -30,11 +30,13 @@ class UnitOfWork[Repository: SQLAlchemyRepository]:
     def __enter__(self) -> t.Self:
         self.session = get_session()
         self.repository = self.repository_type(self.session)
+        # event.listen(self.engine, "before_cursor_execute", self.callback)
 
         return self
 
 
     def __exit__(self, *args) -> None:
+        # event.remove(self.session.bind, "before_cursor_execute", self.callback)
         self.rollback()
         self.session.close()
 
@@ -45,33 +47,3 @@ class UnitOfWork[Repository: SQLAlchemyRepository]:
 
     def rollback(self) -> None:
         self.session.rollback()
-
-
-class SQLalchemyUnitOfWork:
-    def __enter__(self):
-        self.session = get_session()
-        self.connection = self.session.connection()
-        self.engine = self.connection.engine
-        self.count = 0
-
-        event.listen(self.engine, "before_cursor_execute", self.callback)
-
-        return self
-
-
-    def __exit__(self, *args):
-        self.session.close()
-        # print(f"Execution_amount: {self.count}")
-        event.remove(self.engine, "before_cursor_execute", self.callback)
-
-    
-    def commit(self) -> None:
-        self.session.commit()
-
-
-    def rollback(self) -> None:
-        self.session.rollback()
-
-
-    def callback(self, *args, **kwargs) -> None:
-        self.count += 1
